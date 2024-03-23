fc7cd5a58249510400877dd30a987c80d49ccf66
Deepak kori <111627339+Deepakkori45@users.noreply.github.com>
2024-03-23 12:58:41 +0530

    Update chatpdf.py

--- chatpdf.py
+++ chatpdf.py
@@ -57,7 +57,7 @@ def get_conversational_chain():
                              temperature=0.3)
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt, generate_kwargs={"max_tokens": 512})
 
     return chain
 
