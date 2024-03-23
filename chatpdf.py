c2da54a9023bf378ff257f5efae9bcb51fc821a3
Deepak kori <111627339+Deepakkori45@users.noreply.github.com>
2024-03-23 12:42:57 +0530

    Update chatpdf.py

--- chatpdf.py
+++ chatpdf.py
@@ -11,8 +11,8 @@ from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+os.getenv("AIzaSyDMLlQUDRw0WV8iXWhVtMQkfXRFLf92aMo")
+genai.configure(api_key=os.getenv("AIzaSyDMLlQUDRw0WV8iXWhVtMQkfXRFLf92aMo"))
 
 
 
